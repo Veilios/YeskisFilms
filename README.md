0bf9d7b2e594f749591f1b1d38d71fab2a365a7f
Alan Mir <alanpaulphotos@gmail.com>
2022-06-13 10:39:51 -0500

    Updated README.md with objectives, and re styled the home page

--- README.md
+++ README.md
@@ -5,4 +5,15 @@ This site is going to be built using the following technologies:
     - Vue.js
     - ScrollMagic
 
+## Objectives
+    - Add langauages tab into navbar as an option to switch entire site to
+      to a different langauages
+    
+    - Create contact form with functioning email function
 
+    - Create About page
+
+    - Create Gallery page that will have some of his favorite photos and The most
+      recent youtube video
+
+    
--- YeskisFilms/src/views/HomeView.vue
+++ YeskisFilms/src/views/HomeView.vue
@@ -1,9 +1,24 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="App">
+    <transition class="fade">
+    <h1>Yeskis Films</h1>
+    </transition>
+  </div>
 </template>
+
+<style>
+.App {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+}
+
+h1 {
+  display: flex;
+  align-items: center;
+  color: white;
+}
+
+
+</style>
